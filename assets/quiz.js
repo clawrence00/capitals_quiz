9433759199107972027ea4541c6ad396172afd6b
clawrence00 <>
2023-04-25 19:38:22 +0000

    javaScript variables added and additonal divs created

---
+++ assets/quiz.js
@@ -0,0 +1,10 @@
+// get elements and assign variables
+const score = document.getElementById('score');
+const welcome = document.getElementById('welcome');
+const question = document.getElementById('question');
+const start = document.getElementById('start');
+const answerA = document.getElementById('answerA');
+const answerB = document.getElementById('answerB');
+const answerC = document.getElementById('answerC');
+const answerD = document.getElementById('answerD');
+const start = document.getElementById('retry');
